db44e90d48fec495276cf893acbc8eb1cd01b51e
desirefallen <p.basalgin@ya.ru>
2025-12-23 06:43:36 +0300

    count test

--- main_test.go
+++ main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCafeNegative(t *testing.T) {
@@ -46,5 +48,39 @@ func TestCafeWhenOk(t *testing.T) {
 		handler.ServeHTTP(response, req)
 
 		assert.Equal(t, http.StatusOK, response.Code)
+	}
+}
+
+func TestCafeCount(t *testing.T) {
+	city := "moscow"
+	handler := http.HandlerFunc(mainHandle)
+
+	requests := []struct {
+		count int
+		want  int
+	}{
+		{0, 0},
+		{1, 1},
+		{2, 2},
+		{100, min(100, len(cafeList[city]))},
+	}
+
+	for _, v := range requests {
+		reqLocation := fmt.Sprintf("/cafe?count=%d&city=%s", v.count, city)
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", reqLocation, nil)
+		handler.ServeHTTP(response, req)
+
+		require.Equal(t, http.StatusOK, response.Code, fmt.Sprintf("count: %d", v.count))
+
+		trimBody := strings.TrimSpace(response.Body.String())
+		var cafesCount int
+		if trimBody == "" {
+			cafesCount = 0
+		} else {
+			cafesCount = len(strings.Split(trimBody, ","))
+		}
+
+		assert.Equal(t, v.want, cafesCount, fmt.Sprintf("count: %d", v.count))
 	}
 }
